0859d9d82f2e5fff14bc4064e0f067b19c4d3155
Kamran Ayub <kamran.ayub@gmail.com>
2012-02-02 09:28:40 -0600

    Converted tabs to spaces

--- app.js
+++ app.js
@@ -9,45 +9,45 @@
  ********************/
 
 var cli     = require('cli'),
-	  http    = require('http'),
-	  request = require('request'),
-	  colors	= require('colors');
+    http    = require('http'),
+    request = require('request'),
+    colors  = require('colors');
 
 cli.main(function (args, options) {
-	console.log("***********".rainbow);
-	console.log("Node Reddit".cyan);
-	console.log("***********\n\n".rainbow);
+  console.log("***********".rainbow);
+  console.log("Node Reddit".cyan);
+  console.log("***********\n\n".rainbow);
 
-	request('http://reddit.com/.json', function (err, res, body) {
-		if (!err && res.statusCode === 200) {
-			var reddit  = JSON.parse(body),
-				  stories = reddit.data.children.map(function (s) { 
-							        return s.data; 
-						        });
-			
-			// Descending score
-			stories.sort(function (a, b) { return b.score - a.score; });
+  request('http://reddit.com/.json', function (err, res, body) {
+    if (!err && res.statusCode === 200) {
+      var reddit  = JSON.parse(body),
+          stories = reddit.data.children.map(function (s) { 
+                      return s.data; 
+                    });
+      
+      // Descending score
+      stories.sort(function (a, b) { return b.score - a.score; });
 
-			stories.forEach(function (story) {
-				var row = "",
-					title = story.title.length > 100
-						    ? story.title.substr(0, 100) + "..." 
-						    : story.title;
+      stories.forEach(function (story) {
+        var row = "",
+          title = story.title.length > 100
+                ? story.title.substr(0, 100) + "..." 
+                : story.title;
 
-				// Build row
-				// [score] [title] [comments] [subreddit]
-				// This sucks
-				row += story.score.toString().green + "\t";
-				row += title.bold
-				row += " (" + story.domain + ")";
-				row += (" /r/" + story.subreddit).cyan;
-				row += "\n\t";
-				row += story.author.grey;			
-				row += " " + (story.num_comments + " comments").italic.yellow;
-				row += "\n";
+        // Build row
+        // [score] [title] [comments] [subreddit]
+        // This sucks
+        row += story.score.toString().green + "\t";
+        row += title.bold
+        row += " (" + story.domain + ")";
+        row += (" /r/" + story.subreddit).cyan;
+        row += "\n\t";
+        row += story.author.grey;     
+        row += " " + (story.num_comments + " comments").italic.yellow;
+        row += "\n";
 
-				console.log(row);
-			});
-		}
-	});
+        console.log(row);
+      });
+    }
+  });
 });
