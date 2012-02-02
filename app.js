713953a47954ab0800637a968e58ad23a538759a
Kamran Ayub <kamran.ayub@gmail.com>
2012-02-02 09:23:39 -0600

    Added message at the top of app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
+/********************
+ * Node-Reddit
+ *
+ * A simple CLI app that reads in Reddit's frontpage and colorizes it
+ *
+ * by Kamran Ayub
+ ********************/
+
 var cli     = require('cli'),
 	http    = require('http'),
 	request = require('request'),
@@ -12,19 +20,23 @@ cli.main(function (args, options) {
 
 	request('http://reddit.com/.json', function (err, res, body) {
 		if (!err && res.statusCode === 200) {
-			var reddit = JSON.parse(body);
-
-			var stories = reddit.data.children
-				.map(function (s) {	return s.data; });
+			var reddit = JSON.parse(body),
+				stories = reddit.data.children.map(function (s) { 
+							return s.data; 
+						  });
 			
+			// Descending score
 			stories.sort(function (a, b) { return b.score - a.score; });
 
 			stories.forEach(function (story) {
 				var row = "",
-					title = story.title.length > 100 ? story.title.substr(0, 100) + "..." : story.title;
+					title = story.title.length > 100
+						  ? story.title.substr(0, 100) + "..." 
+						  : story.title;
 
 				// Build row
 				// [score] [title] [comments] [subreddit]
+				// This sucks
 				row += story.score.toString().green + "\t";
 				row += title.bold
 				row += " (" + story.domain + ")";
@@ -38,5 +50,4 @@ cli.main(function (args, options) {
 			});
 		}
 	});
-
 });
